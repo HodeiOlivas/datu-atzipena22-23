2956fbc3e52bb69e6f64d565a247afc84ab10982
Hodei Olivas <olivas.hodei@uni.eus>
2022-09-22 11:54:04 +0200

    .

---
+++ mendiencsva/src/main/java/com/dambi/MainMenua.java
@@ -0,0 +1,74 @@
+package com.dambi;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MainMenua {
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int aukera = 0;
+        do {
+            System.out.println();
+            System.out.println("MENDIEN MENUA");
+            System.out.println("====================================");
+            System.out.println("1.- Mendien zerrenda ikusi (taula formatuan)");
+            System.out.println("2.- Mendirik altuena bistaratu");
+            System.out.println("3.- Mendiak esportatu (Araba.csv, Bizkaia.csv, Gipuzkoa.csv)");
+            System.out.println("4.- ...");
+            System.out.println("5.- Irten");
+            System.out.println("");
+            System.out.print("Aukeratu zenbaki bat: ");
+            aukera = in.nextInt();
+            switch (aukera) {
+                case 1:
+                    mendienZerrendaIkusi();
+                    break;
+                case 2:
+                    mendiAltuena();
+                    break;
+                case 3:
+                    mendiakEsportatu();
+                    break;
+                case 5:
+                    System.out.println("Eskerrik asko programa hau erabiltzeagatik.");
+                    break;
+                default:
+                    System.out.println("Aukera okerra. Saiatu berriz.");
+            }
+        } while (aukera != 5);
+        in.next();
+    }
+
+    public static void mendienZerrendaIkusi() throws FileNotFoundException {
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("Mendiak.csv")));
+
+            while (s.hasNext()) { 
+              
+                System.out.println(s.next().replace( ";", " | " )); 
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+    }
+
+    
+
+    public static void mendiAltuena() {
+
+    }
+
+    public static void mendiakEsportatu() {
+
+    }
+
+}
