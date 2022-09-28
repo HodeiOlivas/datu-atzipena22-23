f802b7005a536b0e6e42dfb0fa1ce8c640446d97
Holivas <96474092+Holivas@users.noreply.github.com>
2022-09-28 20:41:06 +0100

    proba

--- mendiencsva/src/main/java/com/dambi/MainMenua.java
+++ mendiencsva/src/main/java/com/dambi/MainMenua.java
@@ -5,19 +5,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+import javax.sound.sampled.SourceDataLine;
 import javax.xml.stream.StreamFilter;
 import javax.xml.transform.stream.StreamSource;
 
 
 
 public class MainMenua {
-
+    private static String[] zutabeak;
+    private Scanner in = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
+         Scanner in = new Scanner(System.in);
         int aukera = 0;
         do {
             System.out.println();
@@ -26,8 +29,7 @@ public class MainMenua {
             System.out.println("1.- Mendien zerrenda ikusi (taula formatuan)");
             System.out.println("2.- Mendirik altuena bistaratu");
             System.out.println("3.- Mendiak esportatu (Araba.csv, Bizkaia.csv, Gipuzkoa.csv)");
-            System.out.println("4.- ...");
-            System.out.println("5.- Irten");
+            System.out.println("4.- Irten");
             System.out.println("");
             System.out.print("Aukeratu zenbaki bat: ");
             aukera = in.nextInt();
@@ -41,7 +43,7 @@ public class MainMenua {
                 case 3:
                     mendiakEsportatu();
                     break;
-                case 5:
+                case 4:
                     System.out.println("Eskerrik asko programa hau erabiltzeagatik.");
                     break;
                 default:
@@ -71,101 +73,93 @@ public class MainMenua {
 
     public static void mendiAltuena() throws IOException {
 
+/* 
+        try (BufferedReader s = new BufferedReader(new FileReader("Mendiak.csv"))){
+            int lerroZenbakia = 0;
+            String banatzailea = ";";
+            String[] mendirikAltuena = null;
+            String l;
+            while ((l = s.readLine()) != null) {
+                zutabeak = l.split(banatzailea);
+                if(lerroZenbakia != 0 && (mendirikAltuena == null || Integer.parseInt(mendirikAltuena[1]) < Integer.parseInt(zutabeak[1]))){
+                    mendirikAltuena = zutabeak;
+                }
+                lerroZenbakia++;
+            }
+
+            System.out.println("Mendirik altuena: ");
+            System.out.printf("%10s | %8s | %10s",mendirikAltuena[0],mendirikAltuena[1],mendirikAltuena[2]);
+            s.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Ez da fitxategia aurkitu");
+        }
+    
+
+
+*/
+
+
+
+
+
+
+
+      /*   Scanner s = null;
+        String mendia;
+        int kont;
+        int altuera = 0;
+        String probintzia;
+        String zakarrontzia;
 
         try {
-            String izena = "";
-            String izenaF = "";
-            int altuera = 0;
-            int altueraF = 0;
-            String kokalekua = "";
-            String kokalekuaF = "";
-            Scanner sc = new Scanner(new FileReader("Mendiak.csv"));
-            Scanner sp;
-            sc.useDelimiter(";");
-            
-            while (sc.hasNext()) {
-                izena = sc.next();
-                try {
-                    altuera = sc.nextInt();
-                } catch (Exception e) {
-                    kokalekua = sc.next();
-                }
-                String[] parts = sc.next().split("\n");
-                kokalekua = parts[0];
-
-                izena = parts[1];
-
-                if (altuera > altueraF) {
-                    izenaF = izena;
-                    altueraF = altuera;
-                    kokalekuaF = kokalekua;
-                }
-            }
-            System.out.println(kokalekuaF + "-ko " + izenaF + " mendia da altuena, " + altueraF + "m-ko altuerarekin");
-            sc.close();
-
-        } catch (FileNotFoundException exception) {
-            System.out.println("Fitxategia ez da aurkitu");
-        }
-    }    
-      
-       
-       
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       /* 
-    String mendiakKatea
-       try {
-            FileReader mendia = new FileReader("Mendiak.csv");
-            BufferedReader a = new BufferedReader(mendia);
-            int altuena = 0;
-          
-            while ((mendiakKatea = a.readLine()) != null) {
-                // split-ek String motako array bat itzultzen du, int bihurtzeko cast bat egiten
-                // dugu
+            s = new Scanner(new BufferedReader(new FileReader("Mendiak.csv")));
+           
+           
+            while (s.hasNext()) {
+               // s.next().replace(";", " ");
                 
-                int[] parts = Stream.of(mendiakKatea.split(";")).mapToInt(Integer::parseInt).toArray();
+                s.useDelimiter(";"+" ");
+                
+                mendia = s.next();
+                try{
+                 int itest = Integer.parseInt(s.next()); 
+                 kont = itest;
              
-                for (int i = 0; i < parts.length; i++) {
-               
-                    if (altuena < parts[i]) {
-                        altuena = parts[i];
-                    }
+                if (altuera <= kont ){
+                    altuera = kont;
+                    probintzia = s.next();
+                   
+                } else {
+                   
+                   probintzia = s.next();
                 }
+              
+            } finally {
+                if (s != null) {
+                    
+                }
+         
             }
+            System.out.println(kont);
+            System.out.println(altuera);
+
+        }   
+            
+            
         } finally {
-          if (mendiakKatea != null) {
-                BufferedReader mendia;
-                mendia.close(); 
-                
-            }*/
+            if (s != null) {
+                s.close();
+            }
         }
+    }
 
+      */
     
-
-  
-    
+    }
 
     public static void mendiakEsportatu() {
 
     }
+
 
 }
